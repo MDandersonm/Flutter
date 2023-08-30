c5e9a8a30a0ac5624e89d001c0877a1bf5eae064
KimYeongJIn <125858342+MDandersonm@users.noreply.github.com>
2023-08-30 22:56:35 +0900

    feat: [김영진] 블로그 앱 회원가입UI [FLUTTER-BLOG-01]

--- flutter_blog/lib/components/custom_text_from_field.dart
+++ flutter_blog/lib/components/custom_text_from_field.dart
@@ -2,23 +2,30 @@ import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
 
+  final String hint;
+
+  const CustomTextFormField({required this.hint});
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      decoration: InputDecoration(
-        hintText: "Enter username",
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        errorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        focusedErrorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: TextFormField(
+        decoration: InputDecoration(
+          hintText: "$hint",//hint라 하지말고 이렇게 하는게 좋다
+          //// null이 되면 오류가나지않고 빈공백이 나온다 그래서 이렇게 넣는게 좋음
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
         ),
       ),
     );
--- flutter_blog/lib/pages/user/join_page.dart
+++ flutter_blog/lib/pages/user/join_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/components/custom_elevated_button.dart';
 import 'package:flutter_blog/components/custom_text_from_field.dart';
 
 class JoinPage extends StatelessWidget {
@@ -10,12 +11,31 @@ class JoinPage extends StatelessWidget {
         child: ListView(
           //리스트뷰는 Center로 정렬이안됨 sizedBox로 한다.
           children: [
-            SizedBox(height: 200),
-            CustomTextFormField(),
+            Container(
+              alignment: Alignment.center,
+              height: 200,
+              child: Text(
+                "Sign up",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ),
+            _joinForm(),
           ],
         ),
       ),
     );
   }
-}
 
+  Widget _joinForm() {
+    return Form(
+      child: Column(
+        children: [
+          CustomTextFormField(hint: "Enter Username"),
+          CustomTextFormField(hint: "Enter Password"),
+          CustomTextFormField(hint: "Enter Email"),
+          CustomElevatedButton(text: "회원가입")
+        ],
+      ),
+    );
+  }
+}
